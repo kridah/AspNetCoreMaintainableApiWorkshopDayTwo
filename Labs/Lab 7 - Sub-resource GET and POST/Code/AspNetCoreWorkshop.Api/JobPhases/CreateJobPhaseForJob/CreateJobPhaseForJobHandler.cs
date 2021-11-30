53a956e8f487c89c0d4a3a8d5765de1bebf5859c
Spencer Schneidenbach <spencer@avironsoftware.com>
2021-11-29 20:14:59 -0600

    first commit

---
+++ Labs/Lab 7 - Sub-resource GET and POST/Code/AspNetCoreWorkshop.Api/JobPhases/CreateJobPhaseForJob/CreateJobPhaseForJobHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AspNetCoreWorkshop.Api.Jobs;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreWorkshop.Api.JobPhases.CreateJobPhaseForJob
+{
+    public class CreateJobPhaseForJobHandler : IRequestHandler<CreateJobPhaseForJobRequest, Func<ControllerBase, IActionResult>>
+    {
+        public CreateJobPhaseForJobHandler(
+            WorkshopDbContext workshopDbContext,
+            IMapper mapper
+        )
+        {
+            WorkshopDbContext = workshopDbContext ?? throw new System.ArgumentNullException(nameof(workshopDbContext));
+            Mapper = mapper ?? throw new System.ArgumentNullException(nameof(mapper));
+        }
+
+        public WorkshopDbContext WorkshopDbContext { get; }
+        public IMapper Mapper { get; }
+
+        public async Task<Func<ControllerBase, IActionResult>> Handle(CreateJobPhaseForJobRequest message, CancellationToken cancellationToken)
+        {
+            //TODO: create new Job from message, above, and save new phase to database
+            //TODO: make sure job exists - if it doesn't return Not Found
+            //TODO: return JobId/Id using OkObjectResult
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# RESTful APIs using ASP.NET Core
+
+## Day 2
+
+| Title | Description | Minutes | End Time |
+| --- | --- | --- | --- |
+| Recap of what we did so far, small things | AutoMapper test, searches with POST | 15 minutes | 9:15 AM 
+| Sub-resources overview |   | 15 minutes | 9:30 AM |
+| Ex: Add GET sub-resource |   | 15 minutes | 9:45 AM |
+| Ex: Add POST sub-resource |   | 15 minutes | 10:00 AM |
+| Lab 7 | Sub-resource GET and POST | 30 minutes | 10:30 AM |
+| Break |   | 15 minutes | 10:45 AM |
+| Lab 7 | Review sub-resource GET and POST | 15 minutes | 11:00 AM |
+| Security overview | Security types, policies, roles, claims | 30 minutes | 11:30 PM |
+| Lab 8 | Add authentication to API | 1 hour | 12:30 PM |
+| Lunch |   | 1 hour | 1:30 PM |
+| Versioning and maintenance |   | 30 minutes | 2:00 PM |
+| Lab 9 | Add Swagger to your application | 45 minutes | 2:45 PM | 
+| Deployment and CI | Show off Azure cloud tooling | 15 minutes | 3:00 PM |
+| Break |   | 15 minutes | 3:15 PM |
+| Comparing and contrasting REST/gRPC/GraphQL | | 45 minutes | 4:00 PM |
+| Logging/advanced validation/accessing HTTP request w/in handlers/etc. | | 30 minutes | 4:30 PM |
+| Open discussion |   | 30 minutes | 5:00 PM |
